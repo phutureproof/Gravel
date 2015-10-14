b17cfeb5848c8715f5280bb1352be75488848323
Dale <dale.phutureproof@gmail.com>
2015-10-14 21:07:53 +0100

    Code restructuring

--- app/controllers/pagesController.php
+++ app/controllers/pagesController.php
@@ -1,1 +1,1 @@
-<?phpuse Gravel\Controller;class pagesController extends Controller{    public function home()    {	    echo "hey there";    }    public function about()    {        echo "this is the about page";    }}
+<?phpuse Gravel\Controller;class pagesController extends Controller{    public function home()    {	    $users = User::all();	    echo $users;    }    public function about()    {        echo "this is the about page";    }}
