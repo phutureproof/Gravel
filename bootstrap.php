9fece3b0229df439d83c4adafaa4c02a82dbe0bd
Dale <dale.phutureproof@gmail.com>
2015-10-17 21:03:36 +0100

    More work on template engine

--- bootstrap.php
+++ bootstrap.php
@@ -1,1 +1,24 @@
-<?phpsession_start();define('BASE_DIR', __DIR__);define('APP_DIR', BASE_DIR . '/app');define('MODEL_DIR', APP_DIR . '/models');define('LIB_DIR', BASE_DIR . '/lib');spl_autoload_register(function ($classname) {    $file = str_replace(['\\', '_'], '/', $classname);    if (file_exists(MODEL_DIR . "/{$file}.php")) {        require_once(MODEL_DIR . "/{$file}.php");        return;    }    if (file_exists(LIB_DIR . "/{$file}.php")) {        require_once(LIB_DIR . "/{$file}.php");        return;    }});
+<?php
+
+session_start();
+
+define('BASE_DIR', __DIR__);
+define('APP_DIR', BASE_DIR . '/app');
+define('MODEL_DIR', APP_DIR . '/models');
+define('LIB_DIR', BASE_DIR . '/lib');
+
+spl_autoload_register(function ($classname) {
+
+	$file = str_replace(['\\', '_'], '/', $classname . '.php');
+
+	if (file_exists(MODEL_DIR . "/{$file}")) {
+		require_once(MODEL_DIR . "/{$file}");
+		return;
+	}
+
+	if (file_exists(LIB_DIR . "/{$file}")) {
+		require_once(LIB_DIR . "/{$file}");
+		return;
+	}
+
+});
