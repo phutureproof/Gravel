758cae16d297644926c0fba68110d3f05c557c65
Dale <dale.phutureproof@gmail.com>
2015-10-17 20:39:00 +0100

    More work on template engine

--- app/views/home.php
+++ app/views/home.php
@@ -1,8 +1,28 @@
 @extends('templates/main')
 
 @section('content')
+
 <h1>This is a header, a H1 to be precise!</h1>
 <p>This code is coming from /views/home.php</p>
+
+<?php if (isset($users)): ?>
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($users as $user ): ?>
+		<tr>
+			<td><?= $user->getName; ?></td>
+			<td><?= $user->email; ?></td>
+		</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+<?php endif; ?>
 @endsection
 
 @section('javascript')
