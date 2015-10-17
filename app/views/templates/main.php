5cf9f4d804d102e1581d1fddb79b3e857afcd213
Dale <dale.phutureproof@gmail.com>
2015-10-17 20:11:52 +0100

    More work on template engine

--- app/views/templates/main.php
+++ app/views/templates/main.php
@@ -1,1 +1,11 @@
-<!doctype html><html lang="en">    <head>        <meta charset="UTF-8">        <title>Template Tests</title>    </head>    <body>		@yield('content')    </body></html>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Template Tests</title>
+	</head>
+	<body>
+		@yield('content')
+		@yield('javascript')
+	</body>
+</html>
